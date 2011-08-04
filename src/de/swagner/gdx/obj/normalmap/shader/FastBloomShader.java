3ce6e2065223c1aff79a8b902491d01525c3014e
Stefan Wagner <bompo@web.de>
2011-08-04 11:14:45 +0300

    FastBloom Vertex Shader optimized with glsl-optimizer FastBloom Fragment Shader optimized with glsl-optimizer

--- src/de/swagner/gdx/obj/normalmap/shader/FastBloomShader.java
+++ src/de/swagner/gdx/obj/normalmap/shader/FastBloomShader.java
@@ -3,42 +3,37 @@ package de.swagner.gdx.obj.normalmap.shader;
 public class FastBloomShader {
 
     public static final String mVertexShader =                    
-        "attribute vec4 a_vertex;   \n" +
-        "attribute vec2 a_texCoord;   \n" +
-        "uniform float TexelOffsetX;   \n" +
-        "uniform float TexelOffsetY;   \n" +
-        "varying vec2  TexCoord0;   \n" +
-        "varying vec2  TexCoord1;   \n" +
-        "varying vec2  TexCoord2;   \n" +
-        "void main()   \n" +
-        "{   \n" +
-        "	// Pass through vertex   \n" +
-        "	gl_Position = a_vertex;   \n" +
-        "	   \n" +
-        "	// Calculate texture offsets and pass through	   \n" +
-        "	vec2 offset = vec2(TexelOffsetX, TexelOffsetY);   \n" +
-        "     \n" +
-        "    TexCoord0 = a_texCoord - offset;   \n" +
-        "    TexCoord1 = a_texCoord;   \n" +
-        "    TexCoord2 = a_texCoord + offset;       \n" +
-        "}   \n";
+"varying vec2 TexCoord2; \n" +
+"varying vec2 TexCoord1; \n" + 
+"varying vec2 TexCoord0; \n" +
+"uniform float TexelOffsetY; \n" +
+"uniform float TexelOffsetX; \n" +
+"attribute vec2 a_texCoord; \n" +
+"attribute vec4 a_vertex; \n" +
+"void main () \n" +
+"{ \n" +
+"  gl_Position = a_vertex; \n" +
+"  vec2 tmpvar_1; \n" +
+"  tmpvar_1.x = TexelOffsetX; \n" +
+"  tmpvar_1.y = TexelOffsetY; \n" +
+"  TexCoord0 = (a_texCoord - tmpvar_1); \n" +
+"  TexCoord1 = a_texCoord; \n" +
+"  TexCoord2 = (a_texCoord + tmpvar_1); \n" +
+"} \n";
     
     public static final String mFragmentShader =   
         "#ifdef GL_ES\n" +
         "precision mediump float;\n" +
         "#endif\n" +
-        "uniform sampler2D  sTexture;\n" +
-        "uniform float bloomFactor;\n" +
-        "varying vec2  TexCoord0;\n" +
-        "varying vec2  TexCoord1;\n" +
-        "varying vec2  TexCoord2;\n" +
-        "void main()\n" +
-        "{\n" +
-        "    vec3 color = texture2D(sTexture, TexCoord0).rgb * 0.333333;\n" +
-        "    color = (texture2D(sTexture, TexCoord1).rgb * 0.333333) + color;\n" +
-        "    color = (texture2D(sTexture, TexCoord2).rgb * 0.333333) + color;\n" + 
-        "    gl_FragColor.rgb = color*bloomFactor;\n" +
-        "}\n";
+"varying vec2 TexCoord2;\n" +
+"varying vec2 TexCoord1;\n" +
+"varying vec2 TexCoord0;\n" +
+"uniform float bloomFactor;\n" +
+"uniform sampler2D sTexture;\n" +
+"void main ()\n" +
+"{\n" +
+"  gl_FragColor.xyz = (((texture2D (sTexture, TexCoord2).xyz * 0.333333) + ((texture2D (sTexture, TexCoord1).xyz * 0.333333) + (texture2D (sTexture, TexCoord0).xyz * 0.333333))) * bloomFactor);\n" +
+"} \n";
 }
 
 /** Precision qualifiers gain one frame on milestone but break desktop support an look a bit uglier
